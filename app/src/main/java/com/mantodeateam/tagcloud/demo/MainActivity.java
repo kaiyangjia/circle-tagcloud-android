6aa82e28195efb5fd432e4a415ae60a2e12aff0d
kaiyangjia <kaiyangjia@163.com>
2017-08-09 21:59:25 +0800

    add anim utils, add demo app

--- app/src/main/java/com/mantodeateam/tagcloud/demo/MainActivity.java
+++ app/src/main/java/com/mantodeateam/tagcloud/demo/MainActivity.java
@@ -1,13 +1,30 @@
-package com.mantodeateam.tagcloud;
+package com.mantodeateam.tagcloud.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.mantodeateam.tagcloud.AnimUtils;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView testImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        testOvalAnim();
+    }
+
+    private void initView() {
+        testImage = (ImageView) findViewById(R.id.testImage);
+    }
+
+    private void testOvalAnim() {
+        AnimUtils.circleAnimation(0, 0, 300, 600, testImage);
     }
 }
--- tagcloud-lib/src/main/java/com/mantodeateam/tagcloud/AnimUtils.java
+++ tagcloud-lib/src/main/java/com/mantodeateam/tagcloud/AnimUtils.java
@@ -1,8 +1,48 @@
 package com.mantodeateam.tagcloud;
 
+import android.animation.ValueAnimator;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.RectF;
+import android.util.Log;
+import android.widget.ImageView;
+
 /**
  * Created by jia on 2017/8/9.
  */
 
 public class AnimUtils {
+    private static final String TAG = "AnimUtils";
+
+    public static void circleAnimation(float left, float top, float right, float bottom, final ImageView imageView) {
+        Path path = new Path();
+        RectF rectF = new RectF(left, top, right, bottom);
+        path.addOval(rectF, Path.Direction.CW);
+        final PathMeasure mPathMeasure = new PathMeasure(path, true);
+
+        final float[] mCurrentPosition = new float[2];
+
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, mPathMeasure.getLength());
+        valueAnimator.setDuration(5000);
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // 当插值计算进行时，获取中间的每个值，
+                // 这里这个值是中间过程中的曲线长度（下面根据这个值来得出中间点的坐标值）
+                float value = (Float) animation.getAnimatedValue();
+                Log.i("Value::", value + "");
+                // ★★★★★获取当前点坐标封装到mCurrentPosition
+                // boolean getPosTan(float distance, float[] pos, float[] tan) ：
+                // 传入一个距离distance(0<=distance<=getLength())，然后会计算当前距
+                // 离的坐标点和切线，pos会自动填充上坐标，这个方法很重要。
+                mPathMeasure.getPosTan(value, mCurrentPosition, null);//mCurrentPosition此时就是中间距离点的坐标值
+                // 移动的商品图片（动画图片）的坐标设置为该中间点的坐标
+                imageView.setTranslationX(mCurrentPosition[0]);
+                imageView.setTranslationY(-mCurrentPosition[1]);
+            }
+        });
+        valueAnimator.start();
+    }
+
 }
